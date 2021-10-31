d4915520e8e7076a6616d7286b6b699ce7e4b918
m4rcus <dauntlessstyle31415926@gmail.com>
2021-10-31 16:18:20 +0800

    Initial Commit

---
+++ DUNE/DUNEApp.swift
@@ -0,0 +1,17 @@
+//
+//  DUNEApp.swift
+//  DUNE
+//
+//  Created by User03 on 2021/10/31.
+//
+
+import SwiftUI
+
+@main
+struct DUNEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
